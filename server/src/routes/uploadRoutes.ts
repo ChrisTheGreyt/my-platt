00ec507b44206fb24b97485a615cbfa7d9bfcce3
Chris <chris@omnimeidacast.com>
2024-11-07 04:26:12 -0500

    remove uplaod form build again

--- server/src/routes/uploadRoutes.ts
+++ server/src/routes/uploadRoutes.ts
@@ -1,11 +1,11 @@
 // src/routes/uploadRoutes.ts
 
 import { Router } from 'express';
-import { uploadImage } from '../controllers/uploadController';
-import { upload, uploadToS3 } from '../middleware/uploadMiddleware';
+// import { uploadImage } from '../controllers/uploadController';
+// import { upload, uploadToS3 } from '../middleware/uploadMiddleware';
 
 const router = Router();
 
-router.post('/', upload.single('file'), uploadToS3, uploadImage);
+// router.post('/', upload.single('file'), uploadToS3, uploadImage);
 
 export default router;
