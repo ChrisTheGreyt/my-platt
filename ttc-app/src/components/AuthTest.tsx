7979311bb4743014cb4185d7f94040d78a957a22
Chris <chris@omnimeidacast.com>
2024-11-07 14:19:07 -0500

    update AuthTest.tsx

--- ttc-app/src/components/AuthTest.tsx
+++ ttc-app/src/components/AuthTest.tsx
@@ -4,12 +4,18 @@
 
 import React from 'react';
 import { useGetAuthUserQuery } from '../state/api';
+import { FetchBaseQueryError } from '@reduxjs/toolkit/query';
+
 
 const AuthTest: React.FC = () => {
     const { data, error, isLoading } = useGetAuthUserQuery();
 
     if (isLoading) return <p>Loading...</p>;
-    if (error) return <p>Error: {error.message}</p>;
+    if (error) {
+        const errorMessage = (error as FetchBaseQueryError)?.data?.['message'] || 'An unexpected error occurred';
+        return <p>Error: {errorMessage}</p>;
+
+      }
 
     return (
         <div>
