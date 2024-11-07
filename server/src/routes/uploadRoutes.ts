8641211a65e017ecdb04fe875d4950dafdbd7ba7
Chris <chris@omnimeidacast.com>
2024-11-07 04:34:58 -0500

    remove uplaod form build again x2

--- server/src/routes/uploadRoutes.ts
+++ server/src/routes/uploadRoutes.ts
@@ -1,11 +1,16 @@
 // src/routes/uploadRoutes.ts
 
-import { Router } from 'express';
-// import { uploadImage } from '../controllers/uploadController';
-// import { upload, uploadToS3 } from '../middleware/uploadMiddleware';
+import { Router } from "express";
 
 const router = Router();
 
-// router.post('/', upload.single('file'), uploadToS3, uploadImage);
+if (!process.env.DISABLE_UPLOAD) {
+  // Only require and use upload middleware if DISABLE_UPLOAD is not set
+  const { upload, uploadToS3 } = require("../middleware/uploadMiddleware");
+  const { uploadImage } = require("../controllers/uploadController");
+
+  // Route: POST /upload
+  router.post('/', upload.single('file'), uploadToS3, uploadImage);
+}
 
 export default router;
