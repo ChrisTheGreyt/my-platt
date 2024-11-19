a6cab711381e239c420f9ece0c33794f54ffa3d5
Chris Grey <chrisgrey@Chriss-Mac-mini.local>
2024-11-19 09:52:31 -0500

    api update

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,7 +14,7 @@ import userRoutes from "./routes/userRoutes";
 import teamRoutes from "./routes/teamRoutes";
 import subscriptionRoutes from "./routes/subscriptionRoutes";
 // import uploadRoutes from "./routes/uploadRoutes"; // Import the upload routes
-import { updateUserAfterPayment } from "./controllers/userController";
+import { createUser, updateUserAfterPayment } from "./controllers/userController";
 import path from "path";
 
 /* CONFIGURATIONS */
@@ -57,6 +57,7 @@ app.use("/search", searchRoutes);
 app.use("/users", userRoutes);
 app.use("/teams", teamRoutes);
 app.use('/subscriptions', subscriptionRoutes);
+app.post('/api/users/create-user', createUser);
 app.post('/users/update-after-payment', (req, res) => {
   console.log('POST /users/update-after-payment hit');
   console.log('Request Body:', req.body);
