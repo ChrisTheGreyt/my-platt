6421c3c8c94c7d53267fddb9e6c12e1727bb4acf
Chris Grey <chrisgrey@Mac.lan>
2025-02-24 14:50:43 -0500

    school update : dev 05

--- server/src/routes/userTaskRoutes.ts
+++ server/src/routes/userTaskRoutes.ts
@@ -7,16 +7,8 @@ const prisma = new PrismaClient();
 
 // Add this at the top of the file, after imports
 const corsMiddleware = (req: express.Request, res: express.Response, next: express.NextFunction) => {
-  const origin = req.headers.origin;
-  const allowedOrigins = [
-    'http://localhost:3000',
-    'https://main.d249lhj5v2utjs.amplifyapp.com'
-  ];
-  
-  if (origin && allowedOrigins.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-  
+  // Force production URL
+  res.header('Access-Control-Allow-Origin', 'https://main.d249lhj5v2utjs.amplifyapp.com');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Requested-With');
   res.header('Access-Control-Allow-Credentials', 'true');
