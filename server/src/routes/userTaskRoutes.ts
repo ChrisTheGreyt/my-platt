75aa0022b9a296ce206eb92590c3e9f3983e7b0e
Chris Grey <chrisgrey@Mac.lan>
2025-02-24 05:25:00 -0500

    school update : amplify login fix 5

--- server/src/routes/userTaskRoutes.ts
+++ server/src/routes/userTaskRoutes.ts
@@ -509,4 +509,11 @@ router.get('/users/:userId/time-gated-projects', async (req, res) => {
   }
 });
 
+router.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL || '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
+
 export default router;
