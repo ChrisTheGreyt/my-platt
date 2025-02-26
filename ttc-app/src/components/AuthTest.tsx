aee30c5da5f4ea4bbbac77ff4698e02b61bfb79c
Chris Grey <chrisgrey@Mac.lan>
2025-02-26 15:38:46 -0500

    retro fix 39

--- ttc-app/src/components/AuthTest.tsx
+++ ttc-app/src/components/AuthTest.tsx
@@ -6,16 +6,19 @@ import React from 'react';
 import { useGetAuthUserQuery } from '../state/api';
 import { FetchBaseQueryError } from '@reduxjs/toolkit/query';
 
+interface ErrorResponse {
+    message: string;
+}
 
 const AuthTest: React.FC = () => {
     const { data, error, isLoading } = useGetAuthUserQuery();
 
     if (isLoading) return <p>Loading...</p>;
     if (error) {
-        const errorMessage = (error as FetchBaseQueryError)?.data?.['message'] || 'An unexpected error occurred';
+        const err = error as FetchBaseQueryError;
+        const errorMessage = (err.data as ErrorResponse)?.message || 'An unexpected error occurred';
         return <p>Error: {errorMessage}</p>;
-
-      }
+    }
 
     return (
         <div>
